342c0d64247ff3c99580fc2bc1c3c4ec56fc78a5
Naveen Kumar Lingala <naveenlingala11@gmail.com>
2025-12-30 15:30:47 +0530

    Fix Angular 21 Vercel deployment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts (PROD)
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.uptrixhub.com' // backend later
+  apiBaseUrl: 'https://backend.uptrixhub.online' // backend later
 };
